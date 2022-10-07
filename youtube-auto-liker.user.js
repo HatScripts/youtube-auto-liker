1efb17d8951d3f5a0d5ad50502a4ee9bc33ba05d
HatScripts <nxhlms@gmail.com>
2022-10-07 17:07:55 +1100

    Fix debugger

--- youtube-auto-liker.user.js
+++ youtube-auto-liker.user.js
@@ -3,7 +3,7 @@
 // @name:zh        YouTube自動點讚
 // @name:ja        YouTubeのような自動
 // @namespace      https://github.com/HatScripts/youtube-auto-liker
-// @version        1.3.10
+// @version        1.3.11
 // @description    Automatically likes videos of channels you're subscribed to
 // @description:zh 對您訂閲的頻道視頻自動點讚
 // @description:ja 購読しているチャンネルの動画が自動的に好きです
@@ -78,15 +78,15 @@
     if (!window.console) {
       return () => {}
     }
-    for (const m in console) {
-      if (typeof console[m] === 'function') {
+    Object.getOwnPropertyNames(window.console).forEach(key => {
+      if (typeof window.console[key] === 'function') {
         if (enabled) {
-          this.debug[m] = console[m].bind(window.console, name + ': ')
+          this.debug[key] = window.console[key].bind(window.console, name + ': ')
         } else {
-          this.debug[m] = () => {}
+          this.debug[key] = () => {}
         }
       }
-    }
+    })
     return this.debug
   }
 
